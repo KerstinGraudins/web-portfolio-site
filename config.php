987cb5f543f759d0a9d42918a4ffd21c86ac2c6f
Kerstin Graudins <kgraud01@seattlecentral.edu>
2017-08-10 14:47:43 -0700

    needed files for contactme.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/*
+config.php - will allow us to use the form called simple.php
+*/
+//Here are the keys for the server: kerstingraudins.com
+$siteKey = "6LfhUSwUAAAAAEGQWyTjolE4JezcwxyjIt3Ku-jP";
+$secretKey = "6LfhUSwUAAAAAOVM5jy5KNZfkSXu6IdjYjhgKQWh";
+?>
